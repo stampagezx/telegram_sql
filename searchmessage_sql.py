694565fa81be9973805dacd290d48ce60749a44c
stampagezx <93703811+stampagezx@users.noreply.github.com>
2022-03-28 13:29:53 +0700

    Updated

--- searchmessage_sql.py
+++ searchmessage_sql.py
@@ -1,6 +1,5 @@
 from ast import keyword
 import asyncio
-from datetime import date
 from telethon import TelegramClient
 from telethon import functions, types
 from telethon.tl.functions.messages import SearchRequest
@@ -25,28 +24,10 @@ client = TelegramClient('user', api_id, api_hash)
 client.start()
 
 async def main():
-    search_group = 'BeInCrypto Việt Nam Community' #input("Nhập tên nhóm muốn tìm kiếm từ khoá: ")
-    keyword_search = 'tăng' #input("Nhập từ khoá muốn tìm kiếm: ")
+
+    filled = 'crypto' # input("Nhập vào từ khoá tìm nhóm để search data: ")
+    keyword_search = 'increase'
     filter = InputMessagesFilterEmpty()
-    
-    result = await client(SearchRequest(
-    peer=search_group,          # On which chat/conversation
-    q=keyword_search,           # What to search for
-    filter=filter,              # Filter to use (maybe filter for media)
-    min_date=None,              # Minimum date
-    max_date=None,              # Maximum date
-    offset_id=0,                # ID of the message to use as offset
-    add_offset=0,               # Additional offset
-    limit=120,                  # How many results
-    max_id=0,                   # Maximum message ID
-    min_id=0,                   # Minimum message ID
-    hash=-1435215991210501094,  # access_hash in result.chats         
-    from_id=None                # Who must have sent the message (peer)
-    ))
-    # f = open('myfile.txt',"w", encoding='utf-8')
-    # f.write(result.stringify())
-    
-    filled = input("Nhập vào từ khoá tìm nhóm để search data: ")
 
     # Lấy dữ liệu nhập vào và chuyển thành tuple
     q = ()
@@ -54,14 +35,42 @@ async def main():
     listq.append(filled)
     q = tuple(listq)
 
-    Q1 = "SELECT id_channel, gr_channel_names FROM gr_channels WHERE keyword=%s"
+    Q1 = "SELECT id_channel, access_hashh, gr_channel_names FROM gr_channels WHERE keyword=%s"
+    Q2 = "INSERT INTO search_message_bykeyword (message_id, keyword_search, channel_id, content, create_at) VALUES (%s, %s, %s, %s, %s)"
+
     cursor.execute(Q1, q)
     testfetch = cursor.fetchall()
-    for index in result.messages:
-        i+=1
-        print(i, index.message)
+    for index in testfetch:
+        print(index[2])
+        access_hashh=int(index[1])
+        result = await client(SearchRequest(
+        peer=str(index[2]),         # On which chat/conversation
+        q=keyword_search,           # What to search for
+        filter=filter,              # Filter to use (maybe filter for media)
+        min_date=None,              # Minimum date
+        max_date=None,              # Maximum date
+        offset_id=0,                # ID of the message to use as offset
+        add_offset=0,               # Additional offset
+        limit=120,                  # How many results
+        max_id=0,                   # Maximum message ID
+        min_id=0,                   # Minimum message ID
+        hash= access_hashh,         # access_hash in result.chats         
+        from_id=None                # Who must have sent the message (peer)
+        ))
+        
+        # f = open('myresultfile.txt',"a",encoding = 'utf-8')
+        # f.write(result.stringify())
+        # f.close()
 
-
-
+        # print(result.stringify())
+        i=0
+        for index1 in result.messages:
+            i+=1
+            print(i, index1.message)
+            val = (index1.id, keyword_search, index[0], index1.message, index1.date)
+            cursor.execute(Q2, val)
+            cursor.fetchone()
+            conn.commit()
+        
 with client:
     client.loop.run_until_complete(main())
