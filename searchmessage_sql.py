2dba22e1d0f88cdb72856548d15ed22374484eef
stampagezx <93703811+stampagezx@users.noreply.github.com>
2022-04-01 17:27:52 +0700

    Tìm tin nhắn từ channel_handmade

--- searchmessage_sql.py
+++ searchmessage_sql.py
@@ -25,30 +25,21 @@ client.start()
 
 async def main():
 
-    filled = 'crypto' # input("Nhập vào từ khoá tìm nhóm để search data: ")
     filter = InputMessagesFilterEmpty()
 
-    # Lấy dữ liệu nhập vào và chuyển thành tuple
-    q = ()
-    listq = list(q)
-    listq.append(filled)
-    q = tuple(listq)
-
-    Q1 = "SELECT id_channel, access_hashh, gr_channel_names FROM gr_channels WHERE keyword=%s"
+    Q1 = "SELECT id_channel, access_hashh FROM gr_channel_handmade"
     Q2 = "INSERT INTO search_message_bykeyword (message_id, keyword_search, channel_id, content, create_at) VALUES (%s, %s, %s, %s, %s)"
-    Q3 = "SELECT searchKeyword FROM msg_keywords"
+    Q3 = "SELECT search_channel_names FROM gr_keywords"
 
-    cursor.execute(Q1, q)
-    testfetch = cursor.fetchall()
+    cursor.execute(Q1)
+    fetch1 = cursor.fetchall()
     cursor.execute(Q3)
     fetch2 = cursor.fetchall()
-    for keyword_search in fetch2:
-        for index in testfetch:
-            print(index[2])
-            access_hashh=int(index[1])
+    for keyword_search1 in fetch1:
+        for keyword_search2 in fetch2:
             result = await client(SearchRequest(
-            peer=str(index[2]),         # On which chat/conversation
-            q=keyword_search[0],           # What to search for
+            peer=keyword_search1[0],             # On which chat/conversation
+            q=keyword_search2[0],           # What to search for
             filter=filter,              # Filter to use (maybe filter for media)
             min_date=None,              # Minimum date
             max_date=None,              # Maximum date
@@ -57,16 +48,13 @@ async def main():
             limit=120,                  # How many results
             max_id=0,                   # Maximum message ID
             min_id=0,                   # Minimum message ID
-            hash= access_hashh,         # access_hash in result.chats         
+            hash=keyword_search1[1],         # access_hash in result.chats         
             from_id=None                # Who must have sent the message (peer)
             ))
 
-            # print(result.stringify()) #Try to get form of result
-            i=0
             for index1 in result.messages:
-                i+=1
-                print(i, index1.message)
-                val = (index1.id, keyword_search[0], index[0], index1.message, index1.date)
+                i = i+1
+                val = (index1.id, keyword_search1[0], index1[0], index1.message, index1.date)
                 cursor.execute(Q2, val)
                 cursor.fetchone()
                 conn.commit()
