dcf797dfd7b84595fa6e8825e66247cd83412270
stampagezx <93703811+stampagezx@users.noreply.github.com>
2022-04-03 23:30:29 +0700

    final

--- searchmessage_sql.py
+++ searchmessage_sql.py
@@ -38,26 +38,24 @@ async def main():
     for keyword_search1 in fetch1:
         for keyword_search2 in fetch2:
             result = await client(SearchRequest(
-            peer=keyword_search1[0],             # On which chat/conversation
+            peer=keyword_search1[0],        # On which chat/conversation
             q=keyword_search2[0],           # What to search for
-            filter=filter,              # Filter to use (maybe filter for media)
-            min_date=None,              # Minimum date
-            max_date=None,              # Maximum date
-            offset_id=0,                # ID of the message to use as offset
-            add_offset=0,               # Additional offset
-            limit=120,                  # How many results
-            max_id=0,                   # Maximum message ID
-            min_id=0,                   # Minimum message ID
-            hash=keyword_search1[1],         # access_hash in result.chats         
-            from_id=None                # Who must have sent the message (peer)
+            filter=filter,                  # Filter to use (maybe filter for media)
+            min_date=None,                  # Minimum date
+            max_date=None,                  # Maximum date
+            offset_id=0,                    # ID of the message to use as offset
+            add_offset=0,                   # Additional offset
+            limit=100,                      # How many results
+            max_id=0,                       # Maximum message ID
+            min_id=0,                       # Minimum message ID
+            hash=int(keyword_search1[1]),   # access_hash in result.chats         
+            from_id=None                    # Who must have sent the message (peer)
             ))
 
             for index1 in result.messages:
-                i = i+1
-                val = (index1.id, keyword_search1[0], index1[0], index1.message, index1.date)
+                val = (index1.id, keyword_search2[0], keyword_search1[0], index1.message, index1.date)
                 cursor.execute(Q2, val)
                 cursor.fetchone()
                 conn.commit()
-        
 with client:
     client.loop.run_until_complete(main())
